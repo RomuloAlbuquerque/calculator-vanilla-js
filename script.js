f4f403e074308ac6f1c1550ab533b6337a6c22c8
RomuloAlbuquerque <66214784+RomuloAlbuquerque@users.noreply.github.com>
2022-11-17 16:50:45 -0300

    calculator

---
+++ script.js
@@ -0,0 +1,49 @@
+const operators = ["+", "-", "*", "/"];
+const operatorsDot = ["+", "-", "*", "/", "."];
+const noLeft = [0, "."];
+let clear = null;
+
+const getPanel = () => document.getElementById("panel-text").innerHTML;
+
+const setPanelNum = (num) =>
+  (document.getElementById("panel-text").innerHTML = num);
+
+const setPanelContentNum = (content, num) => {
+  content.length < 10
+    ? (document.getElementById("panel-text").innerHTML = content + num)
+    : console.log("excedeu");
+};
+
+const setPanelWithFunction = (funcao) =>
+  (document.getElementById("panel-text").innerHTML = funcao);
+
+//LIMPAR//////////////////////////////////////////////////////////////////////////////
+const clean = () => (document.getElementById("panel-text").innerHTML = "");
+
+//INSERIR/////////////////////////////////////////////////////////////////////////////
+const insert = (num) => {
+  let content = getPanel();
+  clear ? (content = "") : null;
+  clear = false;
+  
+  content === ""
+    ? operators.includes(num) || noLeft.includes(num)
+      ? clean()
+      : setPanelNum(num)
+    : operatorsDot.includes(content.slice(-1))
+    ? operators.includes(num) || noLeft.includes(num)
+      ? clean()
+      : setPanelContentNum(content, num)
+    : setPanelContentNum(content, num);
+};
+
+//CALCULAR////////////////////////////////////////////////////////////////////////////
+const calcular = () => {
+  clear = true;
+  const content = getPanel();
+  operators.includes(content.slice(-1))
+    ? null
+    : content
+    ? setPanelWithFunction(eval(content))
+    : null;
+};
